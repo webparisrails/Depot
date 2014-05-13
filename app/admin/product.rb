bd9dcb0f0db6365080a7a3b085895307c480fea7
Raphayol <raphael.egeley@gmail.com>
2014-05-13 03:43:43 +0200

    Category model created, activeadmin installed

---
+++ app/admin/product.rb
@@ -0,0 +1,35 @@
+ActiveAdmin.register Product do
+
+  permit_params :category, :name, :price, :availability, :description
+  index do 
+    selectable_column
+    id_column
+    column :categorie, :category
+    column :nom, :name
+    column :description
+    column :prix, :price do |product|
+      number_to_currency product.price, :unit => "€"
+    end
+    column :disponible, :available do |a|
+      status_tag(a.available? ? "OUI" : "NON", (a.available? :ok ))
+    end
+    column :créer, :created_at
+    column :mis_à_jour, :updated_at
+    default_actions
+  end
+
+  
+  # See permitted parameters documentation:
+  # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # permit_params :list, :of, :attributes, :on, :model
+  #
+  # or
+  #
+  # permit_params do
+  #  permitted = [:permitted, :attributes]
+  #  permitted << :other if resource.something?
+  #  permitted
+  # end
+  
+end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,8 @@
 class Product < ActiveRecord::Base
   has_many :line_items
+  belongs_to :category
   before_destroy :ensure_not_referenced_by_any_line_item
+  validates :category, :presence => true
   #...
   private
 # ensure that there are no line items referencing this product

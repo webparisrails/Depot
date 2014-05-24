b15c1ccdf974db3e6888c8bdd746cd56da34422f
Raphayol <raphael.egeley@gmail.com>
2014-05-24 21:16:19 +0200

    address and name add to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140524115713) do
+ActiveRecord::Schema.define(version: 20140524165739) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -105,6 +105,8 @@ ActiveRecord::Schema.define(version: 20140524115713) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
+    t.string   "address"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true

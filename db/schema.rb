d30d6f21e14063c1b0a8ae00f8083635340f25d5
Goldenson <orefice.maxime@gmail.com>
2014-05-24 14:36:37 +0200

    category works

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140513010535) do
+ActiveRecord::Schema.define(version: 20140524115713) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -52,9 +52,9 @@ ActiveRecord::Schema.define(version: 20140513010535) do
   end
 
   create_table "categories", force: true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "name"
   end
 
   create_table "line_items", force: true do |t|
@@ -89,6 +89,8 @@ ActiveRecord::Schema.define(version: 20140513010535) do
     t.datetime "updated_at"
     t.integer  "category_id"
   end
+
+  add_index "products", ["category_id"], name: "index_products_on_category_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false

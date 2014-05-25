28a1ff2c960ea741c043d3bc48def30d83fcafcb
Raphayol <raphael.egeley@gmail.com>
2014-05-25 22:00:41 +0200

    error in order solved

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -34,7 +34,7 @@ class OrdersController < ApplicationController
   # POST /orders
   # POST /orders.json
   def create
-    @order = Order.new(order_params)
+    @order = Order.new()
     @order.add_line_items_from_cart(@cart)
     respond_to do |format|
       if @order.save
@@ -83,6 +83,6 @@ class OrdersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def order_params
-      params.require(:order).permit(:name, :address, :email, :pay_type)
+      params.require(:order).permit(:name, :address, :email)
     end
 end

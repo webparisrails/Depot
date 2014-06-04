d4c153704b8e271c0858a3c2a255e469410b34b9
Goldenson <orefice.maxime@gmail.com>
2014-06-04 05:53:53 +0200

    remove feature mail

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,5 @@
 class StaticPagesController < ApplicationController
   def home
     @contact = Contact.new(params[:contact])
-    if @contact.save
-      OrderNotifier.contact(@contact).deliver
-    end
   end
 end
